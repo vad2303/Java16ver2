d5e7caf250d9a2ae5907933a82eb1765b5f9f115
vad2303 <126171119+vad2303@users.noreply.github.com>
2023-04-28 08:59:01 +0700

    commit note

---
+++ BAITHUCHANHLAP3/HinhHoc/src/HinhTru.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HinhTru extends HinhTron {
+    public float chieuCao;
+
+    public HinhTru() {
+        ten = "Hinh Tru";
+    }
+
+    public void nhapChieuCao() {
+
+        System.out.println("Chieu cao =");
+        try (Scanner sc = new Scanner(System.in)) {
+            chieuCao = sc.nextFloat();
+        }
+    }
+
+    public void tinhTheTich() {
+        tinhDienTich();
+        theTich = dienTich * chieuCao;
+    }
+}
