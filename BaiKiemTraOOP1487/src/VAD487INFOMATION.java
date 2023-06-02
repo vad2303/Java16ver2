45f88f00df846f8a6a69d73ce2751912beba3219
vad2303 <126171119+vad2303@users.noreply.github.com>
2023-06-02 10:03:20 +0700

    VAD487CAU1

---
+++ BaiKiemTraOOP1487/src/VAD487INFOMATION.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public abstract class VAD487INFOMATION {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner();
+        String Name;
+        System.out.print("Nhap thong tin:" + Name);
+        System.out.print("Them thong tin");
+
+    }
+}
